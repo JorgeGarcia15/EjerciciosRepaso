0a55bcbbc22deace28bca09988a80e9300d21a22
JorgeGarcia15 <jordy615g@gmail.com>
2015-11-13 16:34:17 +0100

    Ejercicio de Repaso 5 terminado

---
+++ src/Repaso5.java
@@ -0,0 +1,43 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+
+
+public class Repaso5 extends GraphicsProgram {
+
+	GRect rectangulo;
+	
+	RandomGenerator aleatorio = new RandomGenerator();
+	
+	public void init(){
+		
+		setSize(800,600);
+		rectangulo = new GRect(120, 80);
+		//inserto el "escuchador" del raton
+		addMouseListeners();
+		
+		
+	}
+	
+	public void run(){
+		
+		add(	rectangulo, 
+				getWidth()/2 - rectangulo.getWidth()/2, 
+				getHeight()/2 - rectangulo.getHeight()/2 
+				);
+			
+		
+	}
+	
+	//añado el metodo quee scucha el evento del clic del raton
+	
+	public void mouseClicked (MouseEvent evento){
+		rectangulo.setFilled(true);
+		rectangulo.setFillColor(aleatorio.nextColor());
+		
+	}
+	
+		
+}
